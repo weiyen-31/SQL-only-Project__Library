09b2ed622d2b2e6f6edbe03453335fac7743adae
weiyen-31 <nick0531hwy@gmail.com>
2026-06-10 16:24:46 +0800

    Update Analysis 2: Genre Popularity Analysis.sql

--- Analysis 2: Genre Popularity Analysis.sql
+++ Analysis 2: Genre Popularity Analysis.sql
@@ -1,26 +1,30 @@
 WITH base AS (
         SELECT B.genre,
-                     BR.id AS borrowing_id,
-        		       DATEDIFF('day', CAST(BR.borrow_date AS DATE), CAST(BR.return_date AS
-       DATE)) AS gap_days
+               BR.id AS borrowing_id,
+               DATEDIFF('day', CAST(BR.borrow_date AS DATE), CAST(BR.return_date AS
+               DATE)) AS gap_days
         FROM BOOKS B
         LEFT JOIN BORROWINGS BR ON B.id = BR.book_id
         AND BR.return_date IS NOT NULL
         ),
+        
 CAL_avg_borrowing_duration AS (
         SELECT genre,
-                     COUNT(borrowing_id) AS total_borrowings,
-                     AVG(gap_days) AS avg_borrowing_duration
+               COUNT(borrowing_id) AS total_borrowings,
+               AVG(gap_days) AS avg_borrowing_duration
         FROM base
         GROUP BY genre
         ORDER BY avg_borrowing_duration DESC
         ),
+
 score AS (
        SELECT genre, 
-                    avg_borrowing_duration,
-                    total_borrowings,
-                    NTILE(4)OVER(ORDER BY total_borrowings) AS score
-       FROM CAL_avg_borrowing_duration   ),
+              avg_borrowing_duration,
+              total_borrowings,
+              NTILE(4)OVER(ORDER BY total_borrowings) AS score
+       FROM CAL_avg_borrowing_duration
+        ),
+        
 classification AS (
        SELECT *,
                CASE
@@ -32,6 +36,7 @@ classification AS (
                 END AS class
         FROM score
         )
+        
 SELECT *
 FROM classification
 ORDER BY total_borrowings DESC
